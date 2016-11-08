63334dca663be3218f55fdf0a993d5c8e113473d
Jack Billings <jackbillings@0587341448.wireless.umich.net>
2016-11-07 22:27:28 -0500

    finished twitterhw3a.py

--- twitterhw3a.py
+++ twitterhw3a.py
@@ -6,3 +6,11 @@
 
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")
+import tweepy
+
+auth = tweepy.OAuthHandler("gvCmOm8i5osgs3pG1dcQD0sFT", "rVIj4n0UBqzWg7HwfmYhNZpcCKGpTbFJletZTIZFXxqy2jJhav")
+auth.set_access_token("277831482-xECfpYtnXu2ynTY4CLHDg81UDVIyaYKvmM3oZkEc", "HV4xPevmVgMOthRPLDEeLSfyTmCgSucaPyH1TYKdZaEPb")
+
+api = tweepy.API(auth)
+
+api.update_with_media('homework3.jpg', '#UMSI-206 #Proj3')
--- twitterhw3b.py
+++ twitterhw3b.py
@@ -6,7 +6,20 @@
 # 3) Print the average polarity of the results
 
 # Be prepared to change the search term during demo.
+print("""No output necessary although you can print out a success/failure message if you want to.""")
+import tweepy
+from textblob import TextBlob
 
+auth = tweepy.OAuthHandler("gvCmOm8i5osgs3pG1dcQD0sFT", "rVIj4n0UBqzWg7HwfmYhNZpcCKGpTbFJletZTIZFXxqy2jJhav")
+auth.set_access_token("277831482-xECfpYtnXu2ynTY4CLHDg81UDVIyaYKvmM3oZkEc", "HV4xPevmVgMOthRPLDEeLSfyTmCgSucaPyH1TYKdZaEPb")
+
+api = tweepy.API(auth)
+public_tweets = api.search('UMSI')
+
+for tweet in public_tweets:
+	# print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	print(analysis.sentiment)
 
 print("Average subjectivity is")
 print("Average polarity is")
