3d8538633196aa65c483acb88cc1d6755128e2aa
Jack Billings <jackbillings@Jacks-MacBook-Pro.local>
2016-11-10 20:14:38 -0500

    made changes to madlibhw3.py

--- madlibhw3.py
+++ madlibhw3.py
@@ -12,7 +12,50 @@
 import nltk 
 import random
 
-# import nltk
-nltk.download('punkt')
+# # import nltk
+# nltk.download('punkt')
+from nltk.book import text2
 
 from nltk import word_tokenize,sent_tokenize
+
+debug = True
+# print(text2)
+# # get file from user to make mad lib out of
+# fname = text2 # need a file with this name in directory
+
+# f = open(fname, 'r')
+# para = text2.read()
+
+tokens = text2[:150]
+for i in tokens:
+	print(i, end=' ')
+# print("TOKENS")
+# print(tokens)
+tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
+# print("TAGGED TOKENS")
+# print(tagged_tokens)
+# if debug:
+# 	print ("First few tagged tokens are:")
+# 	for tup in tagged_tokens[:5]:
+# 		print (tup)
+
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective"}
+substitution_probabilities = {"NN":.15,"NNS":.1,"VB":.1,"JJ":.1}
+
+def spaced(word):
+	if word in [",", ".", "?", "!", ":"]:
+		return word
+	else:
+		return " " + word
+
+final_words = []
+
+
+for (word, tag) in tagged_tokens:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))
+
+print ("".join(final_words))
